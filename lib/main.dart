b0feee6967c82ef7df18c3cb41b00b624b7a50f0
Chenath Perera <chenathperera@gmail.com>
2026-08-18 13:10:23 +0530

    Onboarding Screen 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'features/splash/presentation/screens/splash_screen.dart';
+import 'features/onboarding/presentation/screens/onboarding_screen_1.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFF3D8FEF),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const OnboardingScreen1(),
     );
   }
 }
